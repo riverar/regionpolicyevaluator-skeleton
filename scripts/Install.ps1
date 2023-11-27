33ecb377a7417847473972a5c36d65375d4c1a52
Rafael Rivera <rafael@withinwindows.com>
2023-11-26 23:11:58 -0800

    Remove trust level tinkering from scripts

--- scripts/Install.ps1
+++ scripts/Install.ps1
@@ -2,4 +2,3 @@ SetACL.exe -on "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WindowsRuntime\Activatable
 SetACL.exe -on "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WindowsRuntime\ActivatableClassId\Windows.Internal.System.Profile.RegionPolicyEvaluator" -ot reg -actn ace -ace "n:Administrators;p:full"
 
 reg add HKLM\SOFTWARE\Microsoft\WindowsRuntime\ActivatableClassId\Windows.Internal.System.Profile.RegionPolicyEvaluator /v DllPath /t REG_SZ /d $(Resolve-Path "$PSScriptRoot\..\target\debug\component.dll") /f
-reg add HKLM\SOFTWARE\Microsoft\WindowsRuntime\ActivatableClassId\Windows.Internal.System.Profile.RegionPolicyEvaluator /v TrustLevel /t REG_DWORD /d 0 /f
--- scripts/Uninstall.ps1
+++ scripts/Uninstall.ps1
@@ -2,4 +2,3 @@ SetACL.exe -on "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WindowsRuntime\Activatable
 SetACL.exe -on "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WindowsRuntime\ActivatableClassId\Windows.Internal.System.Profile.RegionPolicyEvaluator" -ot reg -actn ace -ace "n:Administrators;p:read"
 
 reg add HKLM\SOFTWARE\Microsoft\WindowsRuntime\ActivatableClassId\Windows.Internal.System.Profile.RegionPolicyEvaluator /v DllPath /t REG_SZ /d "$env:SystemRoot\System32\SystemSettings.DataModel.dll" /f
-reg add HKLM\SOFTWARE\Microsoft\WindowsRuntime\ActivatableClassId\Windows.Internal.System.Profile.RegionPolicyEvaluator /v TrustLevel /t REG_DWORD /d 2 /f
